2e8a2f935f38e1cb909dc1ca8f93ad2d4a35cf87
rekhapanda <rpanda@usc.edu>
2014-03-11 13:12:28 -0700

    Update Fizzbuzz.c

--- Fizzbuzz.c
+++ Fizzbuzz.c
@@ -5,10 +5,10 @@ int main(char *argc,int argv){
   for(i=1;i<=100;i++){
     if((i%3)!=0 && (i%5)!=0)     //checks for not a multiple of 3 or 5
       printf("%d",i);
-    if(i%3==0)
-      printf("Fizz");           //checks for the multiples of 3
-    if(i%5==0)
-      printf("Buzz");           //checks for the multiples of 5
+    if(i%3==0)                  //checks for the multiples of 3
+      printf("Fizz");           
+    if(i%5==0)                  //checks for the multiples of 5
+      printf("Buzz");           
     printf("\n");
   }
   return 0;
